3332649007b3fc78ece83bf877a74d702b3de8b9
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-06 19:56:19 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// Set elementos html
+const campoBusca = document.querySelector("#campo-busca");
+const botaoBusca = document.querySelector("#botao-busca");
+
+const campoResultados = document.querySelector(".resultados");
+
+// Criando log
+botaoBusca.addEventListener(
+  "click",
+  () => {
+    buscarDadosNaApi(campoBusca.value);
+  },
+  true
+);
+
+// Função de busca
+function buscarDadosNaApi(nomePokemon) {
+  let resultados = fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`);
+  resultados.then(
+    (Response) => {
+      if (Response.ok) {
+        Response.json().then(exibirResultados, erroNaChamada);
+      } else {
+        erroNaChamada(Response.status);
+      }
+    },
+    () => erroNaChamada("Promise não retornou")
+  );
+}
+
+function exibirResultados(resultado) {
+  let resultadoFormatado = document.createElement("div");
+
+  resultadoFormatado.innerHTML = `
+    <h3>${resultado.name}</h3>
+    <img src=${resultado.sprites?.back_default}></img>
+    `;
+
+  campoResultados.appendChild(resultadoFormatado);
+}
+
+function erroNaChamada(erro) {
+  console.error(erro);
+}
