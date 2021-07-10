b8f871472f10e9195168eba0a8aa4105f2e4d89a
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-10 15:27:02 -0300

    url imagem erro é armazenada em variavel

--- js/view/ErrorView.js
+++ js/view/ErrorView.js
@@ -4,16 +4,18 @@ class ErrorView extends View {
   }
 
   template(Response) {
+    const errorImageSrc = "../../assets/error.svg";
+
     if (Response.status == 404)
       return `
         <h2>Erro 404:</h2>
         <p>Nenhum pokémon encontrado</p>
-        <img src="../../assets/error.svg"></img>
+        <img src=${errorImageSrc}></img>
       `;
 
     return `
     <h2>Erro ${Response.status}:</h2>
-    <img src="../../assets/error.svg"></img>
+    <img src=${errorImageSrc}></img>
   `;
   }
 }
