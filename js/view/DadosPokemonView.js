baaadadcd5a86a30e584f0ab462074cbf8bca8a8
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-10 10:59:06 -0300

    é exibido o id do pokemon

--- js/view/DadosPokemonView.js
+++ js/view/DadosPokemonView.js
@@ -7,9 +7,16 @@ class DadosPokemonView {
     this.elemento.innerHTML = this.template(pokemon);
   }
 
+  /**
+   *
+   * Retorna um HTML pronto para exibição no painel escolhido.
+   * @param {Pokemon} pokemon
+   * @return {InnerHTML}
+   * @memberof DadosPokemonView
+   */
   template(pokemon) {
     return `
-    <h2>${pokemon.nomeOcidental}</h2>
+    <h2>Nº:${pokemon.id} / ${pokemon.nomeOcidental}</h2>
     <img src=${pokemon.spriteFrontal}></img>
     <p>Tipo: ${pokemon.tipo1}${pokemon.tipo2}</p>
     <p>Peso: ${pokemon.peso}</p>
