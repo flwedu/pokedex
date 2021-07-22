4637d47e462b066ddd89776bd6122829fbbbed06
Eduardo Aquino <55285816+flwedu@users.noreply.github.com>
2021-07-22 18:28:38 -0300

    fix ,,,,,, before table using join('')

--- js/view/StatsPokemonView.js
+++ js/view/StatsPokemonView.js
@@ -4,10 +4,10 @@ class StatsPokemonView extends View {
       <p>Stats Iniciais do pokémon</p>
         <table class='stats-table'>
         <thead>
-        ${pokemon.statsLabel.map((stats) => `<td>${stats}</td>`)}
+        ${pokemon.statsLabel.map((stats) => `<td>${stats}</td>`).join('')}
         </thead>
         <tbody>
-        ${pokemon.stats.map((valor) => `<td>${valor}</td>`)}
+        ${pokemon.stats.map((valor) => `<td>${valor}</td>`).join('')}
         </tbody>
         </table>
         `;
