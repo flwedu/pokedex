29fa8278b4c857ed10367e5ffda8d4f1ddd57bbb
Eduardo <edutraquino@gmail.com>
2021-09-27 21:36:36 -0300

    fix all uppercase text being not formatted

--- js/util/text.js
+++ js/util/text.js
@@ -1,6 +1,8 @@
 // Classe responsável por funções de manipulação de texto
 function formatText(texto) {
   let primeiraLetra = texto.slice(0, 1).toUpperCase();
-  let restanteDoTexto = texto.slice(1, texto.length);
+  let restanteDoTexto = texto.slice(1, texto.length).toLowerCase();
   return primeiraLetra.concat("", restanteDoTexto);
 }
+
+module.exports = formatText;
