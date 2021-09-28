41ca08cd809f46c8789885426a34e0a9fd16b02e
Eduardo <edutraquino@gmail.com>
2021-09-27 21:36:10 -0300

    add formatText tests

---
+++ test/util/text.test.js
@@ -0,0 +1,13 @@
+const formatText = require("../../js/util/text");
+
+test("function should format to uppercase only the first letter of a full lowcase text", () => {
+  expect(formatText("eeve")).toBe("Eeve");
+});
+
+test("function should format to uppercase only the first letter of a full uppercase text", () => {
+  expect(formatText("PIKACHU")).toBe("Pikachu");
+});
+
+test("function should format to uppercase only the first letter of a text", () => {
+  expect(formatText("Pikachu")).toBe("Pikachu");
+});
