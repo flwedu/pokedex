25e9fd8d8d71b0ed1992b77fb0466b810191d641
Eduardo <edutraquino@gmail.com>
2021-09-28 21:28:08 -0300

    writting new tests

--- test/model/pokemon.test.js
+++ test/model/pokemon.test.js
@@ -1,12 +1,27 @@
 import Pokemon from "../../js/model/Pokemon";
 import { dittoData } from "../TestData.js";
 
-test("should create a Pokemon right id", () => {
-  let pokemon = new Pokemon(dittoData);
-  expect(pokemon.id).toBe(dittoData.id);
+describe("Creating a pokemon", () => {
+  test("should throw an error while receiving empty data", () => {
+    expect(() => {
+      const pokemon = new Pokemon();
+    }).toThrowError();
+  });
+
+  test("create a pokemon object", () => {
+    const pokemon = new Pokemon(dittoData);
+    expect(pokemon).toBeInstanceOf(Pokemon);
+  });
 });
 
-test("should create a Pokemon with right name", () => {
-  let pokemon = new Pokemon(dittoData);
-  expect(pokemon.nomeOcidental).toBe(dittoData.name);
+describe("Checking pokemon attributes", () => {
+  test("should create a Pokemon right id", () => {
+    const pokemon = new Pokemon(dittoData);
+    expect(pokemon.id).toBe(dittoData.id);
+  });
+
+  test("should create a Pokemon with right name", () => {
+    const pokemon = new Pokemon(dittoData);
+    expect(pokemon.nomeOcidental).toBe(dittoData.name);
+  });
 });
