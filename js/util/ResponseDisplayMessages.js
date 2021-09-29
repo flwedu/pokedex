e28105268a75a9ef4852bce68a955bfb126ff71b
Eduardo <edutraquino@gmail.com>
2021-09-29 17:40:40 -0300

    add "" to image url of sprites

--- js/util/ResponseDisplayMessages.js
+++ js/util/ResponseDisplayMessages.js
@@ -16,7 +16,7 @@ const errorMessages = {
 export function sucessTextWithPokemonData(pokemon) {
   return `
     <h2>Nº:${pokemon.id} / ${pokemon.nomeOcidental}</h2>
-    <img src=${pokemon.spriteFrontal}></img>
+    <img src="${pokemon.spriteFrontal}"></img>
     <p>Tipo: ${pokemon.tipo1}${pokemon.tipo2}</p>
     <p>Peso: ${pokemon.peso}</p>
     `;
