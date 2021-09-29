3eca38c7cd07afa58ec6022205d6f70ba95c6e30
Eduardo <edutraquino@gmail.com>
2021-09-29 17:06:44 -0300

    fix response string to be rendered

--- js/view/ErrorView.js
+++ js/view/ErrorView.js
@@ -8,14 +8,23 @@ const errorMessages = {
   500: "Server Error",
 };
 export default class ErrorView extends View {
-  update(Response) {
-    this._resultsHtmlElement.innerHTML = this.template(Response);
+  /**
+   *
+   * @param {Response} response
+   */
+  update(response) {
+    this._resultsHtmlElement.innerHTML = this.template(response);
   }
 
-  template(Response) {
+  /**
+   *
+   * @param {Response} response
+   * @returns {string} literal string.
+   */
+  template(response) {
     return `
-        <h2>Erro:</h2>
-        <p>${errorMessages[Response.status] | errorMessages.default}</p>
+        <h2>Error ${response.status}:</h2>
+        <p>${errorMessages[response.status] || errorMessages.default}</p>
         <img src=${errorImageSrc}></img>
       `;
   }
