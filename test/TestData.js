edbed453bd50fc94013a0a3736dcfb3b4544f673
Eduardo <edutraquino@gmail.com>
2021-09-28 21:14:18 -0300

    export some variables to a new file

---
+++ test/TestData.js
@@ -0,0 +1,72 @@
+export const dittoData = {
+  id: 132,
+  name: "Ditto",
+  sprites: null,
+  weight: 40,
+  stats: [
+    {
+      base_stat: 48,
+      effort: 1,
+      stat: {
+        name: "hp",
+        url: "https://pokeapi.co/api/v2/stat/1/",
+      },
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "attack",
+        url: "https://pokeapi.co/api/v2/stat/2/",
+      },
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "defense",
+        url: "https://pokeapi.co/api/v2/stat/3/",
+      },
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "special-attack",
+        url: "https://pokeapi.co/api/v2/stat/4/",
+      },
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "special-defense",
+        url: "https://pokeapi.co/api/v2/stat/5/",
+      },
+    },
+    {
+      base_stat: 48,
+      effort: 0,
+      stat: {
+        name: "speed",
+        url: "https://pokeapi.co/api/v2/stat/6/",
+      },
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "normal",
+        url: "https://pokeapi.co/api/v2/type/1/",
+      },
+    },
+  ],
+};
+
+export const HtmlWithDittoData = `
+    <h2>Nº:132 / Ditto</h2>
+    <img src="">
+    <p>Tipo: normal</p>
+    <p>Peso: 40</p>
+    `;
--- test/view/DadosPokemonView.test.js
+++ test/view/DadosPokemonView.test.js
@@ -1,78 +1,6 @@
 import Pokemon from "../../js/model/Pokemon.js";
 import DadosPokemonView from "../../js/view/DadosPokemonView.js";
-
-const dittoData = {
-  id: 132,
-  name: "Ditto",
-  sprites: null,
-  weight: 40,
-  stats: [
-    {
-      base_stat: 48,
-      effort: 1,
-      stat: {
-        name: "hp",
-        url: "https://pokeapi.co/api/v2/stat/1/",
-      },
-    },
-    {
-      base_stat: 48,
-      effort: 0,
-      stat: {
-        name: "attack",
-        url: "https://pokeapi.co/api/v2/stat/2/",
-      },
-    },
-    {
-      base_stat: 48,
-      effort: 0,
-      stat: {
-        name: "defense",
-        url: "https://pokeapi.co/api/v2/stat/3/",
-      },
-    },
-    {
-      base_stat: 48,
-      effort: 0,
-      stat: {
-        name: "special-attack",
-        url: "https://pokeapi.co/api/v2/stat/4/",
-      },
-    },
-    {
-      base_stat: 48,
-      effort: 0,
-      stat: {
-        name: "special-defense",
-        url: "https://pokeapi.co/api/v2/stat/5/",
-      },
-    },
-    {
-      base_stat: 48,
-      effort: 0,
-      stat: {
-        name: "speed",
-        url: "https://pokeapi.co/api/v2/stat/6/",
-      },
-    },
-  ],
-  types: [
-    {
-      slot: 1,
-      type: {
-        name: "normal",
-        url: "https://pokeapi.co/api/v2/type/1/",
-      },
-    },
-  ],
-};
-
-const expectedHtmlWithPokemonData = `
-    <h2>Nº:132 / Ditto</h2>
-    <img src="">
-    <p>Tipo: normal</p>
-    <p>Peso: 40</p>
-    `;
+import { dittoData, HtmlWithDittoData } from "../TestData.js";
 
 describe("Should create a View with pokemon data", () => {
   test("Should throw error while passing incorrect CSS selector information", () => {
@@ -96,6 +24,6 @@ describe("Should render the correct pokemon data", () => {
 
     view.update(new Pokemon(dittoData));
 
-    expect(htmlElement.innerHTML).toBe(expectedHtmlWithPokemonData);
+    expect(htmlElement.innerHTML).toBe(HtmlWithDittoData);
   });
 });
