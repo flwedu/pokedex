92dc56c17283a0952dea5c973504f476462a78a8
Eduardo <edutraquino@gmail.com>
2021-09-30 16:47:14 -0300

    add tests

---
+++ test/controller/SearchController.test.js
@@ -0,0 +1,11 @@
+import SearchController from "../../js/controller/SearchController.js";
+
+describe("Should create a controller", () => {
+  test.todo("Creating with valid informations");
+
+  test.todo("Check attributes");
+});
+
+describe("Calling methods", () => {
+  test.todo("search method should be called");
+});
