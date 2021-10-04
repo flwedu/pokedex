4f51e0f786601cfa5bebd2fde25b04e3386f4f2c
Eduardo <edutraquino@gmail.com>
2021-10-04 17:02:03 -0300

    refactor and add searchNext and searchPrevious

--- js/controller/SearchController.js
+++ js/controller/SearchController.js
@@ -8,25 +8,53 @@ export default class SearchController {
    * @param {ViewController} viewController
    */
   constructor(viewController) {
+    this.lastSearched = 0;
     this._viewController = viewController;
   }
 
   /**
-   * Executes a Call to the scearch service
+   * Executes a Call to the scearch service and updates the view with results
+   * @param {string | number} param
+   */
+  doTheApiSearchAndUpdateControllers(param) {
+    searchInAPI(param)
+      .then((response) => {
+        let pokemon = new Pokemon(response);
+        this._viewController.setPokemonExibido(pokemon);
+        this._viewController.updateSelectedView();
+        this.lastSearched = pokemon.id;
+      })
+      .catch((errorResponse) => {
+        this._viewController.renderErrorView(errorResponse);
+        console.error(errorResponse);
+      });
+  }
+  /**
+   * Receives the HtmlInput text and call a method to executes a Call to the scearch service
    * @param {HTMLInputElement} searchInputHtmlElement
    */
   search(searchInputHtmlElement) {
     if (searchInputHtmlElement.value.length < 1) {
       return;
     }
-    searchInAPI(searchInputHtmlElement.value.toLowerCase())
-      .then((response) => {
-        this._viewController.setPokemonExibido(new Pokemon(response));
-        this._viewController.updateSelectedView();
-      })
-      .catch((errorResponse) => {
-        this._viewController.renderErrorView(errorResponse);
-        console.error(errorResponse);
-      });
+    this.doTheApiSearchAndUpdateControllers(
+      searchInputHtmlElement.value.toLowerCase()
+    );
+  }
+
+  /**
+   * Search data for the next pokemon
+   */
+  searchNext() {
+    this.doTheApiSearchAndUpdateControllers(this.lastSearched + 1);
+  }
+
+  /**
+   * Search data for the previous pokemon
+   */
+  searchPrevious() {
+    if (this.lastSearched > 1) {
+      this.doTheApiSearchAndUpdateControllers(this.lastSearched - 1);
+    }
   }
 }
