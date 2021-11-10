b4c59d5fe4200cc050d2a3be92279fe6ee66fd58
Eduardo Aquino <edutraquino@gmail.com>
2021-11-10 19:02:51 -0300

    generated dist

--- dist/js/controller/AutoCompleteController.js
+++ dist/js/controller/AutoCompleteController.js
@@ -1,35 +1,28 @@
-import AutoCompleteService from "../service/AutoCompleteService.js";
-import AutoCompleteDrawer from "../view/AutoCompleteDrawer.js";
+import AutoCompleteService from "../service/AutoCompleteService";
+import AutoCompleteDrawer from "../view/AutoCompleteDrawer";
 const dataPath = "./data/pokemon_names.json";
 /**
  * Classe que cuida das ações da janela de preview de nomes digitados.
  */
 export default class AutoCompleteController {
-  constructor(resulsHtmlElement, searchInputElement) {
-    this._autoCompleteDrawer = new AutoCompleteDrawer(
-      resulsHtmlElement,
-      searchInputElement
-    );
-    this._searchInputElement = searchInputElement;
-    this._autoCompleteService = new AutoCompleteService(dataPath);
-  }
-  /**
-   *
-   * Método que solicita que o controller busque os resultados por nome e atualize a view
-   * @param {string} name
-   * @memberof AutoCompleteController
-   */
-  searchAndUpdateView() {
-    this._autoCompleteDrawer.update(
-      this._autoCompleteService.listarPorAproximacao(
-        this._searchInputElement.value
-      )
-    );
-  }
-  /**
-   * Solicita o fechamento da lista.
-   */
-  closeList() {
-    this._autoCompleteDrawer.closeList();
-  }
+    constructor(resulsHtmlElement, searchInputElement) {
+        this._autoCompleteDrawer = new AutoCompleteDrawer(resulsHtmlElement, searchInputElement);
+        this._searchInputElement = searchInputElement;
+        this._autoCompleteService = new AutoCompleteService(dataPath);
+    }
+    /**
+     *
+     * Método que solicita que o controller busque os resultados por nome e atualize a view
+     * @param {string} name
+     * @memberof AutoCompleteController
+     */
+    searchAndUpdateView() {
+        this._autoCompleteDrawer.update(this._autoCompleteService.listarPorAproximacao(this._searchInputElement.value));
+    }
+    /**
+     * Solicita o fechamento da lista.
+     */
+    closeList() {
+        this._autoCompleteDrawer.closeList();
+    }
 }
--- dist/js/util/ResponseDisplayMessages.js
+++ dist/js/util/ResponseDisplayMessages.js
@@ -1,5 +1,5 @@
-import { statsLabel } from "../model/Pokemon.js";
-import formatText from "./text.js";
+import { statsLabel } from "../model/Pokemon";
+import formatText from "./text";
 const errorImageSrc = "../../assets/error.svg";
 const errorMessages = new Map();
 errorMessages.set("default", "Error");
@@ -11,19 +11,17 @@ errorMessages.set("500", "Server Erro");
  * @returns {string}
  */
 export function sucessTextWithPokemonData(pokemon) {
-  const parentElement = document.createElement("div");
-  const h2 = document.createElement("h2");
-  const img = document.createElement("img");
-  const p = document.createElement("p");
-  const pw = document.createElement("p");
-  h2.textContent = `Nº:${pokemon.id} / ${formatText(pokemon.name)}`;
-  img.src = pokemon.sprites.front_default;
-  p.textContent = `Type: ${pokemon.types
-    .map((type) => type.type.name)
-    .join("|")}`;
-  pw.textContent = `Weight: ${pokemon.weight} (in hectograms)`;
-  parentElement.append(h2, img, p, pw);
-  return parentElement.innerHTML;
+    const parentElement = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const pw = document.createElement("p");
+    h2.textContent = `Nº:${pokemon.id} / ${formatText(pokemon.name)}`;
+    img.src = pokemon.sprites.front_default;
+    p.textContent = `Type: ${pokemon.types.map(type => type.type.name).join("|")}`;
+    pw.textContent = `Weight: ${pokemon.weight} (in hectograms)`;
+    parentElement.append(h2, img, p, pw);
+    return parentElement.innerHTML;
 }
 /**
  * Function that returns a literal string with pokemon stats data to be used in the element inner text
@@ -31,21 +29,21 @@ export function sucessTextWithPokemonData(pokemon) {
  * @returns {string}
  */
 export function sucessTextWithPokemonStats(pokemon) {
-  const parentElement = document.createElement("div");
-  const p = document.createElement("p");
-  const table = document.createElement("table");
-  p.textContent = "Initial Stats of Pokémon";
-  table.className = "stats-table";
-  table.innerHTML = `
+    const parentElement = document.createElement("div");
+    const p = document.createElement("p");
+    const table = document.createElement("table");
+    p.textContent = "Initial Stats of Pokémon";
+    table.className = "stats-table";
+    table.innerHTML = `
   <thead>
     ${statsLabel.map((stats) => `<td>${stats}</td>`).join("")}
   </thead>
   <tbody>
-    ${pokemon.stats.map((stat) => `<td>${stat.base_stat}<td>`)}
+    ${pokemon.stats.map(stat => `<td>${stat.base_stat}<td>`)}
   </tbody>
   `;
-  parentElement.append(p, table);
-  return parentElement.innerHTML;
+    parentElement.append(p, table);
+    return parentElement.innerHTML;
 }
 /**
  *
@@ -53,16 +51,13 @@ export function sucessTextWithPokemonStats(pokemon) {
  * @returns {string}
  */
 export function errorMessageWithResponseCode(response) {
-  const parentElement = document.createElement("div");
-  const h2 = document.createElement("h2");
-  const p = document.createElement("p");
-  const img = document.createElement("img");
-  h2.textContent = `Error ${response.status}`;
-  p.textContent = `${
-    errorMessages.get(response.status.toString()) ||
-    errorMessages.get("default")
-  }`;
-  img.src = errorImageSrc;
-  parentElement.append(h2, p, img);
-  return parentElement.innerHTML;
+    const parentElement = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+    h2.textContent = `Error ${response.status}`;
+    p.textContent = `${errorMessages.get(response.status.toString()) || errorMessages.get("default")}`;
+    img.src = errorImageSrc;
+    parentElement.append(h2, p, img);
+    return parentElement.innerHTML;
 }
