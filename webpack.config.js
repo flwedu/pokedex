50f4882046d1e95bbf27deeced31ea95e12e9c56
Eduardo Aquino <edutraquino@gmail.com>
2021-11-10 18:55:03 -0300

    add copy-webpack and html-webpack plugins

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./dist/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build/app"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename: "index.html",
+      hash: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./dist/css",
+          to: "css",
+        },
+        {
+          from: "./assets",
+          to: "assets",
+        },
+        {
+          from: "./data",
+          to: "data",
+        },
+      ],
+    }),
+  ],
+};
