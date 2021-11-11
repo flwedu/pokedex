6854078de673727f0d7412d4f39961f60577a458
Eduardo <edutraquino@gmail.com>
2021-11-11 19:15:09 -0300

    add playTransition()

--- src/view/updateViewFunctions.ts
+++ src/view/updateViewFunctions.ts
@@ -5,14 +5,23 @@ import { errorMessageWithResponseCode, sucessTextWithPokemonData, sucessTextWith
 export const renderPokemonData = (pokemon: IPokemon) => {
 
     display__data.innerHTML = sucessTextWithPokemonData(pokemon);
+    playTransition();
 }
 
 export const renderPokemonStats = (pokemon: IPokemon) => {
 
     display__data.innerHTML = sucessTextWithPokemonStats(pokemon);
+    playTransition();
 }
 
 export const renderWithError = (response: Response) => {
 
     display__data.innerHTML = errorMessageWithResponseCode(response);
+    playTransition();
+}
+
+function playTransition() {
+    display__data.classList.add("emtransicao");
+    setTimeout(() => display__data.classList.remove("emtransicao"), 1000);
+
 }
