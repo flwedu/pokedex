b323ca197ad148214a0c69343fd71d26887cc038
Eduardo <edutraquino@gmail.com>
2021-11-10 21:29:27 -0300

    export references to dom elements

--- src/ui/Buttons.ts
+++ src/ui/Buttons.ts
@@ -1,3 +1,8 @@
-export const buttonPrevious = document.getElementById("navigation-previous");
-export const buttonNext = document.getElementById("navigation-next");
-export const searchButton = document.getElementById("botao-busca");
+import { EventEmitter } from "../core/EventEmitter";
+import { searchTextField } from "./DomElements";
+
+const buttonPrevious = document.getElementById("navigation-previous") as HTMLButtonElement;
+const buttonNext = document.getElementById("navigation-next") as HTMLButtonElement;
+const buttonSearch = document.getElementById("botao-busca") as HTMLButtonElement;
+
+export { buttonNext, buttonPrevious, buttonSearch }
--- src/ui/DomElements.ts
+++ src/ui/DomElements.ts
@@ -1,1 +1,23 @@
-export const searchTextField = document.getElementById("search-text");
+import { EventEmitter } from "../core/EventEmitter";
+
+const searchTextField = document.getElementById("search-text") as HTMLInputElement;
+const display__data = document.querySelector(".display__data") as HTMLDivElement;
+
+// Monitorando teclas digitadas no campo de texto
+searchTextField.addEventListener("keydown", (event) => {
+    if (keyMapper.has(event.key.toString())) keyMapper.get(event.key.toString())();
+    else keyMapper.get("Default")();
+});
+
+// Mapeamento de todas as keys e ações disparadas
+const keyMapper = new Map();
+keyMapper.set("Enter", () => EventEmitter.emit("search", searchTextField.value));
+keyMapper.set("Default", () => EventEmitter.emit("updateAutoComplete", searchTextField.value));
+keyMapper.set("Backspace", () => {
+    //@ts-ignore
+    if (searchTextField.value.length < 2) {
+        EventEmitter.emit("closeAutoComplete", null);
+    }
+})
+
+export { searchTextField, display__data }
