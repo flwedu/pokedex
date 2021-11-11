3bff44639210840f03cf69912171b014d923be27
Eduardo <edutraquino@gmail.com>
2021-11-10 21:16:23 -0300

    created EventEmitter

---
+++ src/core/EventEmitter.ts
@@ -0,0 +1,18 @@
+export const EventEmitter = {
+    events: new Map<string, Function[]>(),
+
+    on: (topic: string, callback: Function) => {
+        const oldEvents = EventEmitter.events.get(topic);
+        if (EventEmitter.events.has(topic)) {
+            EventEmitter.events.set(topic, [...oldEvents, callback]);
+        }
+        return EventEmitter.events.set(topic, [callback]);
+    },
+
+    emit: (topic: string, data: any) => {
+        const listeners = EventEmitter.events.get(topic);
+        if (Array.isArray(listeners) && listeners.length) {
+            listeners.forEach((event) => event(data));
+        }
+    },
+};
