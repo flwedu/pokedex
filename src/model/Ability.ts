88939eb9eda63d48f6b85450284170e0cabd8045
Eduardo Aquino <edutraquino@gmail.com>
2021-11-18 17:43:23 -0300

    creating ability interface

---
+++ src/model/Ability.ts
@@ -0,0 +1,14 @@
+export interface IAbility {
+
+    id: number,
+    name: string,
+    effect_entries: [
+        {
+            effect: string,
+            language: {
+                name: string,
+                url: string
+            }
+        }
+    ]
+}
