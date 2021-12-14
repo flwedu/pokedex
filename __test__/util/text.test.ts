ad42a5ea935e3345d64244ee2c3eac1f5869c031
Eduardo <edutraquino@gmail.com>
2021-12-14 17:58:43 -0300

    add tests

--- __test__/util/text.test.ts
+++ __test__/util/text.test.ts
@@ -1,14 +1,35 @@
-import formatText from "../../src/util/text"
+import { formatFirstLetterToUppercase, removeInvalidCharacters } from "../../src/util/text"
 
-describe("Format text functions", () => {
+describe("Format text case functions", () => {
 
-    test("Formatting the text correctly", () => {
+    it("First letter Uppercase", () => {
 
-        expect(formatText("aaa")).toBe("Aaa");
+        expect(formatFirstLetterToUppercase("abcd")).toBe("Abcd");
 
-        expect(formatText("AAA")).toBe("Aaa");
+        expect(formatFirstLetterToUppercase("ABCD")).toBe("Abcd");
 
-        expect(formatText("Aaa")).toBe("Aaa");
+        expect(formatFirstLetterToUppercase("Efgh")).toBe("Efgh");
+    })
+})
+
+describe("Remove invalid characters functions", () => {
+
+    it("Doesn't removes any character", () => {
+
+        expect(removeInvalidCharacters("pikachu")).toBe("pikachu");
+        expect(removeInvalidCharacters("eeve")).toBe("eeve");
     })
 
+    it("Removing first caracter if is equal to 0", () => {
+
+        expect(removeInvalidCharacters("020")).toBe(20);
+        expect(removeInvalidCharacters("001")).toBe(1);
+    })
+
+    it("Returning only lowercase string", () => {
+
+        expect(removeInvalidCharacters("Pikachu")).toBe("pikachu");
+        expect(removeInvalidCharacters("dItto")).toBe("ditto");
+        expect(removeInvalidCharacters("CHARMANDER")).toBe("charmander");
+    })
 })
