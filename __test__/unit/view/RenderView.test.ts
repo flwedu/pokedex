b7d63e017146ba1587561761b1b7a6193303598a
Eduardo <edutraquino@gmail.com>
2021-12-16 20:56:41 -0300

    add tests to render view functions

---
+++ __test__/unit/view/RenderView.test.ts
@@ -0,0 +1,31 @@
+import { ditto } from "../../__mock__/ditto"
+import { RenderView } from "../../../src/view/RenderView"
+
+const display = document.createElement("div");
+const render = new RenderView(display)
+const pokemonMockData = ditto;
+const function1 = () => `<div>function1</div>`;
+const function2 = () => `<div>function2</div>`;
+render.viewFunctions = [function1, function2];
+
+
+it("Should render the innerHTML of the display HTML Element", async () => {
+
+    await render.renderView(pokemonMockData);
+
+    expect(display.innerHTML).toEqual(function1());
+})
+
+it("Should render the innerHTML with the next function data", async () => {
+
+    await render.renderNextView(pokemonMockData);
+
+    expect(display.innerHTML).toEqual(function2());
+})
+
+it("Should render the innerHTML of the display HTML Element with previous function data", async () => {
+
+    await render.renderpreviousView(pokemonMockData);
+
+    expect(display.innerHTML).toEqual(function1());
+})
