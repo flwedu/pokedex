46c82b51b1aa80f3fe9ecc0f1906098cfacabcf2
Eduardo Aquino <edutraquino@gmail.com>
2022-07-09 17:12:02 -0300

    style: fix typo and ident code

--- src/util/text.ts
+++ src/util/text.ts
@@ -1,15 +1,15 @@
 export function formatFirstLetterToUppercase(texto: string) {
-
   const [firstLetter, ...rest] = texto;
-  return rest.reduce((previus, next) => previus + next.toLowerCase(), firstLetter.toUpperCase());
+  return rest.reduce(
+    (prev, next) => prev + next.toLowerCase(),
+    firstLetter.toUpperCase()
+  );
 }
 
 export function removeInvalidCharacters(text: string) {
-
   const number = Number.parseInt(text);
   if (!Number.isNaN(number)) return number;
 
   const symbolRegex = /[^a-zA-Z]/g;
-  return text.toLowerCase().replace(symbolRegex, '');
-
+  return text.toLowerCase().replace(symbolRegex, "");
 }
