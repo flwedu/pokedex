e3f15847c2414a4113f11a4edfaae6ea3441c50f
Eduardo Aquino <edutraquino@gmail.com>
2022-07-09 17:36:45 -0300

    refact: renaming new elements references

--- src/view/ui/DomElements.ts
+++ src/view/ui/DomElements.ts
@@ -3,8 +3,7 @@ const getById = (id: string) => document.getElementById(id);
 const DomElements = {
   inputSearch: getById("input__search") as HTMLInputElement,
   divResults: getById("display__data") as HTMLDivElement,
-  divAutoComplete: getById("display__autocomplete") as HTMLDivElement,
-  divNavigationButtons: getById("navigation__buttons") as HTMLDivElement,
+  divNavigationButtons: getById("navigation") as HTMLDivElement,
 };
 
 export default DomElements;
