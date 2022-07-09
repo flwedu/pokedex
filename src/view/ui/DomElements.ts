dba8e512c0837bd3e213cea07d6743948e67e12c
Eduardo Aquino <edutraquino@gmail.com>
2022-07-09 17:24:24 -0300

    refact: rename navigation buttons and add dots

--- src/view/ui/DomElements.ts
+++ src/view/ui/DomElements.ts
@@ -4,7 +4,7 @@ const DomElements = {
   inputSearch: getById("input__search") as HTMLInputElement,
   divResults: getById("display__data") as HTMLDivElement,
   divAutoComplete: getById("display__autocomplete") as HTMLDivElement,
-  divNavigationButtons: getById("navigation") as HTMLDivElement,
+  divNavigationButtons: getById("navigation__buttons") as HTMLDivElement,
 };
 
 export default DomElements;
