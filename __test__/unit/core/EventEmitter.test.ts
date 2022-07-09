add97230deb53a85fbab6831d06fe5675210ff32
Eduardo Aquino <edutraquino@gmail.com>
2022-07-08 21:59:12 -0300

    fix: typo

--- __test__/unit/core/EventEmitter.test.ts
+++ __test__/unit/core/EventEmitter.test.ts
@@ -1,19 +1,19 @@
-import { EventEmitter } from "../../../src/core/EventEmitter"
+import { EventEmitter } from "../../../src/core/EventEmitter";
 
-it("Should add a listner", () => {
+it("Should add a listener", () => {
+  EventEmitter.on("A", () => {
+    return;
+  });
 
-    EventEmitter.on("A", () => { return });
-
-    expect(EventEmitter.events.has("A")).toBe(true);
-})
+  expect(EventEmitter.events.has("A")).toBe(true);
+});
 
 it("Should trigger the function", () => {
+  const testFunction = jest.fn().mockImplementation((args) => args);
 
-    const testFunction = jest.fn().mockImplementation((args) => args);
+  EventEmitter.on("A", (args: any) => testFunction(args));
+  EventEmitter.emit("A", "Test");
 
-    EventEmitter.on("A", (args: any) => testFunction(args));
-    EventEmitter.emit("A", "Test");
-
-    expect(testFunction).toHaveBeenCalledTimes(1);
-    expect(testFunction).toHaveBeenCalledWith("Test");
-})
+  expect(testFunction).toHaveBeenCalledTimes(1);
+  expect(testFunction).toHaveBeenCalledWith("Test");
+});
