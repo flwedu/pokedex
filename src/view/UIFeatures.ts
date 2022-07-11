210e17fa500eafff4aafe33331dc9f27dd5da1b4
Eduardo Aquino <edutraquino@gmail.com>
2022-07-10 22:50:52 -0300

    feat: add name suggestion methods to ui features

--- src/view/UIFeatures.ts
+++ src/view/UIFeatures.ts
@@ -2,6 +2,8 @@ import IScreen from "./screen";
 
 export class UIFeatures {
   public dots: HTMLInputElement[];
+  private pokemonNames: string[] = [];
+  private suggestionDataList: HTMLDataListElement;
 
   initNavigationDots(htmlEl: HTMLDivElement, successScreen: IScreen[]) {
     this.dots = successScreen.map((_, index) => {
@@ -17,5 +19,31 @@ export class UIFeatures {
   changeActiveNavigationDot(id: number) {
     this.dots.forEach((dot) => dot.classList.remove("active"));
     if (id > -1) this.dots[id].classList.add("active");
+  }
+
+  initNameSuggestion(htmlEl: HTMLDataListElement) {
+    this.suggestionDataList = htmlEl;
+
+    fetch("../../assets/data/pokemon_names.json")
+      .then((res) => res.json())
+      .then((data: { id: string; name: string }[]) => {
+        this.pokemonNames = data.map((pokemon) => pokemon.name);
+      });
+  }
+
+  updateNameSuggestion(query: string) {
+    const renderOption = (name: string) => {
+      const option = document.createElement("option");
+      option.value = name;
+      this.suggestionDataList.appendChild(option);
+    };
+
+    this.suggestionDataList.innerHTML = "";
+    if (query.length > 0) {
+      const filteredNames = this.pokemonNames
+        .filter((name) => name.toLowerCase().includes(query.toLowerCase()))
+        .slice(0, 5);
+      filteredNames.forEach(renderOption);
+    }
   }
 }
